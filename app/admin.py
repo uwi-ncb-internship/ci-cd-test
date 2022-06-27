59058f2489395095a3e5d88d712ee5f68a8a9f18
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-06-27 00:57:10 -0500

    hard code username for now

--- app/admin.py
+++ app/admin.py
@@ -4,6 +4,6 @@ admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 @admin_bp.route('/')
 def admin_dashboard():
-    admin_username = session['username']
+    admin_username = 'ylynfatt' #session['username']
 
     return render_template('admin/dashboard.html', username=admin_username)
