8bb98d5c265a2fafe3659a7b2fcb0efd7c83478d
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-06-27 00:58:05 -0500

    edit function docstring to fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,14 +16,16 @@ def app():
 @pytest.fixture
 def client(app):
     """
-    The client fixture calls app.test_client() with the application object created by the app fixture. 
-    Tests will use the client to make requests to the application without running the server.
+    The client fixture calls app.test_client() with the application object 
+    created by the app fixture. Tests will use the client to make requests 
+    to the application without running the server.
     """
     return app.test_client()
 
 @pytest.fixture
 def runner(app):
     """
-    The runner fixture is similar to client. app.test_cli_runner() creates a runner that can call the Click commands registered with the application.
+    The runner fixture is similar to client. app.test_cli_runner() creates 
+    a runner that can call the Click commands registered with the application.
     """
     return app.test_cli_runner()
