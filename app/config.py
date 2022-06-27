8cbd814ee2ae46faae47ca84fee79b5784255fb7
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-06-27 01:25:43 -0500

    add class docstring

--- app/config.py
+++ app/config.py
@@ -8,4 +8,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config():
+    """ Config class which will contain all configuration variables for application """
     SECRET_KEY=os.environ.get('SECRET_KEY')
