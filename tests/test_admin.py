14e6d4d0ca50955cbe8702c1f25550b5eb98b4c4
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-06-27 10:32:49 -0500

    added new test

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -9,3 +9,9 @@ def test_admin_dashboard(client):
     response = client.get('/admin/')
     assert response.status_code == 200
     assert b'Admin Dashboard' in response.data
+
+def test_admin_users(client):
+    """Test that we can load the admin dashboard."""
+    response = client.get('/admin/users/')
+    assert response.status_code == 200
+    assert b'yannick' in response.data  
