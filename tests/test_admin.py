addb57d254fd2defe78603de704e56230889c642
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-06-27 10:39:33 -0500

    removed extra whitespace

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -14,4 +14,4 @@ def test_admin_users(client):
     """Test that we can load the admin dashboard."""
     response = client.get('/admin/users')
     assert response.status_code == 200
-    assert b'test' in response.data  
+    assert b'test' in response.data
