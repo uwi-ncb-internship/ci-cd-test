ef8893f80f27ccd3042b78bffd1aa1f195643d2c
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-06-27 10:35:58 -0500

    fixed linting issues and test

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -12,6 +12,6 @@ def test_admin_dashboard(client):
 
 def test_admin_users(client):
     """Test that we can load the admin dashboard."""
-    response = client.get('/admin/users/')
+    response = client.get('/admin/users')
     assert response.status_code == 200
-    assert b'yannick' in response.data  
+    assert b'test' in response.data  
